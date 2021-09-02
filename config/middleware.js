05c314d52e84f3314b0bbe2421a2e6afe8e84433
Paul McKenna <mckennapaul27@gmail.com>
2021-09-02 18:20:58 +0100

    updates

--- config/middleware.js
+++ config/middleware.js
@@ -6,6 +6,7 @@ module.exports = {
                 'http://localhost:3000',
                 'http://localhost:1337',
                 'https://portfolio-website-fe.vercel.app',
+                'https://portfolio-admin-cms.herokuapp.com',
                 'https://mysite.com',
                 'https://www.mysite.com',
             ],
