689e02494385705d6b4e29f906a2ffe4fcd679e0
Paul McKenna <mckennapaul27@gmail.com>
2021-09-02 13:43:52 +0100

    updated

---
+++ config/middleware.js
@@ -0,0 +1,14 @@
+module.exports = {
+    //...
+    settings: {
+        cors: {
+            origin: [
+                'http://localhost:3000',
+                'http://localhost:1337',
+                'https://portfolio-website-fe.vercel.app',
+                'https://mysite.com',
+                'https://www.mysite.com',
+            ],
+        },
+    },
+};
